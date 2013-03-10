35c4931e4e35f4e61b5752c84bddf9c4fa2dfc4c
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-03-10 16:26:42 -0300

    Change css to bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-#= require jquery
-#= require jquery_ujs
-#= require twitter/bootstrap/bootstrap-transition
-#= require twitter/bootstrap/bootstrap-alert
-#= require twitter/bootstrap/bootstrap-modal
-#= require twitter/bootstrap/bootstrap-button
-#= require twitter/bootstrap/bootstrap-collapse
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require_tree .
