3d21b1a2801ff421afdf0cd470ceac81b51906d3
Juarez Lustosa <juarez.lustosa@gmail.com>
2013-03-10 16:34:30 -0300

    Views for locations

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+function reloadLocation() {
+  if ($("tr").closest(".where_iam")) {
+    setInterval(loadLocations, 1000);
+  };
+};
+
+function loadLocations(){
+  $.ajax({
+   type: "GET",
+    url: "change_locations"
+  });
+}
+
+reloadLocation();
